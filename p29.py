f2731ef141d0040784295c160c30ca021c31ba46
Spaceman Nor Bear <simon.buck@gstt.nhs.uk>
2020-07-22 11:34:33 +0100

    Add files via upload

---
+++ p29.py
@@ -0,0 +1,13 @@
+a = 8
+b = 4
+print( 'Assign Values:\t\t', 'a =', a, '\tb =', b , type( a ) )
+a += b
+print( 'Add & Assign:\t\t', 'a+=b = ', a , type( a ) )
+a -= b
+print( 'Subtract & Assign:\t', 'a-=b = ', a , type( a ) )
+a *= b
+print( 'Multiply & Assign:\t', 'a*=b = ', a , type( a ) )
+a /=b
+print( 'Divide & Assign:\t', 'a/=b = ', a , type( a ) )
+a %= b
+print( 'Modulo & Assign:\t', 'a%=b = ', a , type( a ) )
