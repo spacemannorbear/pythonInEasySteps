5a3aecc8e6d086da01b849fd1d27a6b5f17731ab
Spaceman Nor Bear <simon.buck@gstt.nhs.uk>
2020-07-22 22:28:29 +0100

    Add files via upload

---
+++ p31.py
@@ -0,0 +1,12 @@
+nil = 0
+num = 0
+max = 1
+cap = 'A'
+low = 'a'
+print( 'Equality:\t', nil, '==', num, '\t', nil == num )
+print( 'Equality:\t', cap, '==', low, '\t', cap == low )
+print( 'Inequality:\t', nil, '!=', max, '\t', nil != max )
+print( 'Greater:\t', nil, '>', max, '\t\t', nil > max )
+print( 'Lesser:\t\t', nil, '<', max, '\t\t', nil < max )
+print( 'More or equal:\t', nil, '>=', num, '\t', nil >= num )
+print( 'Less or equal:\t', max, '<=', num, '\t', max <= num )
